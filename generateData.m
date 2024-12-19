ce5f17970b2aa6aa290818e7d43653d4b82c2a14
lucasjeaybizot <70509037+lucasjeaybizot@users.noreply.github.com>
2024-12-18 18:52:58 -0800

    Update generateData.m

--- generateData.m
+++ generateData.m
@@ -1,10 +1,16 @@
 clear
 
-data_cube = nan(2,2048,100);
-    
+data_cube = nan(2,2048,100); % initiate empty 3D matrix
+
 for j = 1:100
-    data_cube(1,:,j) = [zeros(1,1024) linspace(0,-4,1024)];
-    if j<50  
+    % Set the pseudo-EEG signal
+    data_cube(1,:,j) = [zeros(1,1024) linspace(0,-4,1024)]; % set the exact same pseudo-RP for all trials
+
+    % Set the phase signal per trial
+    % At each if-statement the last item added inside the circshift sets the phase locking of the selected trials
+    % 2048 sets the sampling rate
+    
+    if j<50  % select 50 percent of trials
             data_cube(2,:,j) = circshift(linspace(-pi,pi,2048), round(2048/12) + 0);
     elseif j>=50 & j<60
             data_cube(2,:,j) = circshift(linspace(-pi,pi,2048), round(2048/12) + round(2048/6));
@@ -19,6 +25,7 @@ for j = 1:100
     end
 end
 
+% Copy this simulated data 50 times for 50 pseudo participants
 for i = 1:50
     save(sprintf('simul%02d_cube.mat', i), 'data_cube');
 end
